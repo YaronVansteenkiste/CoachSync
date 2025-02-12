c49c0c4ac055b2b9d2f075b0126fc70c5c6d6c1c
Yaron Vansteenkiste <125354929+YaronVansteenkiste@users.noreply.github.com>
2025-02-12 18:59:45 +0100

    12/02: removing unused imports, creating basic folders and pages with TODO

---
+++ app/account/login/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <ul>
+            <h1>
+                Login TODO
+            </h1>
+        </ul>
+    )
+}
---
+++ app/account/register/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <ul>
+            <h1>
+                Register TODO
+            </h1>
+        </ul>
+    )
+}

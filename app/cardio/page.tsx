5229bc3eb330d85bf38ffc003eae7eefe3214a41
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-02-12 18:45:07 +0100

    12/02: creating pages

---
+++ app/cardio/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <ul>
+            <h1>
+                Your Cardio Activity
+            </h1>
+        </ul>
+    )
+}
---
+++ app/strength/page.tsx
@@ -0,0 +1,9 @@
+export default async function Page() {
+    return (
+        <ul>
+            <h1>
+                Progress Chart TODO
+            </h1>
+        </ul>
+    )
+}

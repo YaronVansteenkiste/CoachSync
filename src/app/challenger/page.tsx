cdfb9e8c64d7caeff7251c79c1166b2a56e2144e
YaronVansteenkiste <yaron.vansteenkiste@telenet.be>
2025-03-21 10:09:30 +0100

    21/03: spotlights

--- src/app/challenger/page.tsx
+++ src/app/challenger/page.tsx
@@ -1,22 +1,20 @@
 'use client'
-import React, { useEffect, useState } from 'react';
+import { calculateRank, getLeaderboard, getUserData } from "@/app/actions/ranking/calculateRank";
 import { Card } from "@/components/ui/card";
 import { Progress } from "@/components/ui/progress";
-import { Button } from "@/components/ui/button";
-import { calculateRank, getUserData, getLeaderboard } from "@/app/actions/ranking/calculateRank";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
 import { authClient } from "@/lib/auth/client";
-import { useRouter } from "next/navigation";
-import { Table, TableHead, TableRow, TableCell, TableBody, TableCaption, TableHeader } from "@/components/ui/table";
-import Image from 'next/image';
+import { cn } from '@/lib/utils';
 import BoringAvatar from 'boring-avatars';
+import Image from 'next/image';
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from 'react';
 
 export default function ChallengerPage() {
   const {
     data: session,
-    isPending,
-    error,
-    refetch
-  } = authClient.useSession();
+    isPending  } = authClient.useSession();
   const router = useRouter();
 
   const [userId, setUserId] = useState<string | null>(null);
@@ -63,25 +61,41 @@ export default function ChallengerPage() {
   return (
     <div className="space-y-8">
       <h1 className="text-4xl font-bold">Challenger</h1>
-      <Card className="shadow-lg p-6">
-        <div className="flex justify-center">
-          {userRank[0]?.image && (
-            <div>
-              <Image src={userRank[0].image} alt={userRank[0]?.name} width={150} height={150} />
-              <h1 className="font-bold text-center">{userRank[0]?.name.toUpperCase()}</h1>
-            </div>
+      <Card className="shadow-lg p-6 relative overflow-hidden">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
           )}
-        </div>
-        
-        <div className="flex justify-end">
-          <span className="text-sm text-gray-500">
-            {isMaxLevel ? "Max Level" : `Experience required till ${nextRank[0]?.name}`}
-          </span>
-        </div>
-        <Progress value={progressValue} max={100} className={`my-4 ${isMaxLevel ? 'bg-gold' : ''}`} />
-        <div className="flex justify-between">
-          <span className="text-sm text-gray-500">0%</span>
-          <span className="text-sm text-gray-500">{isMaxLevel ? "100%" : `${progressValue.toFixed(2)}%`}</span>
+        />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+
+        <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500 opacity-30 rounded-full blur-3xl z-0"></div>
+        <div className="absolute top-1/3 right-1/4 w-96 h-96 bg-purple-500 opacity-30 rounded-full blur-3xl z-0"></div>
+        <div className="absolute bottom-1/4 left-1/3 w-96 h-96 bg-red-500 opacity-30 rounded-full blur-3xl z-0"></div>
+
+        <div className="relative z-10">
+          <div className="flex justify-center">
+            {userRank[0]?.image && (
+              <div className="flex flex-col items-center">
+                <Image src={userRank[0].image} alt={userRank[0]?.name} width={150} height={150} />
+                <TextGenerateEffect words={userRank[0]?.name.toUpperCase()} className="font-bold text-center" />
+              </div>
+            )}
+          </div>
+
+          <div className="flex justify-end">
+            <span className="text-sm text-gray-500">
+              {isMaxLevel ? "Max Level" : `Experience required till ${nextRank[0]?.name}`}
+            </span>
+          </div>
+          <Progress value={progressValue} max={100} className={`my-4 ${isMaxLevel ? 'bg-gold' : ''}`} />
+          <div className="flex justify-between">
+            <span className="text-sm text-gray-500">0%</span>
+            <span className="text-sm text-gray-500">{isMaxLevel ? "100%" : `${progressValue.toFixed(2)}%`}</span>
+          </div>
         </div>
       </Card>
 
